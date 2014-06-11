6a22ad60078e6412fcba3aea48e25aa86cb756c4
David Brown <ddb@bithead.net>
2014-06-11 10:16:57 -0700

    Initial Commit

---
+++ Stacks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stacks
+//
+//  Created by David Brown on 6/11/14.
+//  Copyright (c) 2014 bithead. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
